3ab791738ab0aaacdaeff9ce0e001ec29f65e573
AntoineLemarchand <antoine.lemarchand05@gmail.com>
2022-11-07 10:47:31 +0100

    react subfolders

--- containers/react-transcendance/src/component/App.tsx
+++ containers/react-transcendance/src/component/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from '../logo.svg';
+import '../static/App.css';
+import Nav from './Nav'
 
 function App() {
   return (
     <div className="App">
+		<Nav></Nav>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
