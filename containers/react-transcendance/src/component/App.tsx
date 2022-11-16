0c1462f65b49d96f81b53e3768b3e33430b70bb0
AntoineLemarchand <antoine.lemarchand05@gmail.com>
2022-11-16 14:27:06 +0100

    feat(App.tsx): login route

--- containers/react-transcendance/src/component/App.tsx
+++ containers/react-transcendance/src/component/App.tsx
@@ -1,11 +1,19 @@
 import * as React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Main from './Main'
 import Login from './Login'
 
 function App() {
+	//const [token, setToken] = useState();
 	return (
-			<Main></Main>
-			//<Login></Login>
+			<div className="App">
+				<BrowserRouter>
+					<Routes>
+						<Route path="/" element={<Login />}/>
+						<Route path="/dashboard" element={<Main />}/>
+					</Routes>
+				</BrowserRouter>
+			</div>
 			)
 }
 
