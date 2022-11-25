b8d7bdecf25a98d3b6e8cc4cb00b776710800eeb
AntoineLemarchand <antoine.lemarchand05@gmail.com>
2022-11-25 13:49:32 +0100

    feat(app.controller.spec.tsx): signin tests

--- containers/nest-transcendance/src/app.controller.spec.ts
+++ containers/nest-transcendance/src/app.controller.spec.ts
@@ -31,6 +31,7 @@ describe('AuthController', () => {
 		await app.init();
 	})
 
+// LOGIN
 	it("should return 401 on wrong password", async () => {
 		return loginUser("Thomas", "wrong password")
 		.then((response)=>expect(response.status).toBe(401))
@@ -49,8 +50,27 @@ describe('AuthController', () => {
 		})
 	});
 
+// SIGNIN
 	it("should return 401 when creating an already existent user", async () => {
 		return signinUser("Thomas", "test")
 		.then((response)=>expect(response.status).toBe(401))
 	});
+
+	it("should return 201 and a token when creating user", async () => {
+		return signinUser("JayDee", "yeah")
+		.then((response)=> {
+			expect(response.status).toBe(201)
+			expect(response.body.access_token).toBeDefined()
+		})
+	});
+
+	it("should return a token on login of a newly created user", async () => {
+		signinUser("JayDee", "yeah")
+		return loginUser("JayDee", "yeah")
+		.then((response)=> {
+			expect(response.status).toBe(201)
+			expect(response.body.access_token).toBeDefined()
+		})
+	});
+
 })
