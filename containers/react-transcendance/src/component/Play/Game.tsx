05225a7faa8284bf1c947247ea3f00fc7307e7b0
AntoineLemarchand <antoine.lemarchand05@gmail.com>
2022-12-02 10:36:59 +0100

    fix(package.json): restored from conflict

--- containers/react-transcendance/src/component/Play/Game.tsx
+++ containers/react-transcendance/src/component/Play/Game.tsx
@@ -9,11 +9,7 @@ function Game() {
   const boardSpeed = 10;
   const [pLeftPos, setPLeftPos] = useState(37.5);
   const [pRightPos, setPRightPos] = useState(37.5);
-  const [ballPos, setBallPos] = useState({x: 48.5, y: 48.5});
-
-  const updateBallPosition = () => {
-    setBallPos({x: ballPos.x++, y: ballPos.y})
-  }
+  const [ballPos, setBallPos] = useState({x: 10, y: 10});
 
   useEffect(() => {
     window.addEventListener("keypress", keypressHandler, false);
@@ -28,7 +24,7 @@ function Game() {
     else if (event.code === "KeyK" && pLeftPos > 0)
       setPLeftPos(pLeftPos - boardSpeed < 0 ? 0 : pLeftPos - boardSpeed)
     else if (event.code === "KeyA")
-      setBallPos({x: 50, y: 50});
+      setBallPos({x: 90, y: 90});
   }
 
   const ballStyle = {
