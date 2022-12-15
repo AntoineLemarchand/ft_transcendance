6a38e6bd76c719b4337e665182ad7cba7b0845bf
Frederik Schlüter <frederik.schlueter@posteo.de>
2022-12-15 15:53:41 +0100

    BREAKING CHANGE: fetch all usernames by on route /user/matchingNames/

--- containers/nest-transcendance/src/user/user.controller.ts
+++ containers/nest-transcendance/src/user/user.controller.ts
@@ -1,62 +1,72 @@
 /* eslint-disable prettier/prettier */
 import {
-    Controller,
-    Delete,
-    Get,
-    Post,
-    Request,
-    UseGuards,
-    Param,
+  Controller,
+  Delete,
+  Get,
+  Post,
+  Request,
+  UseGuards,
+  Param
 } from '@nestjs/common';
-import {UserService} from './user.service';
-import {JwtAuthGuard} from '../auth/jwt.auth.guard';
+import { UserService } from './user.service';
+import { JwtAuthGuard } from '../auth/jwt.auth.guard';
 
 @Controller()
 export class UserController {
-    constructor(private userService: UserService) {
-    }
+  constructor(private userService: UserService) {
+  }
 
-    @UseGuards(JwtAuthGuard)
-    @Post('friend')
-    async addFriend(@Request() req: any) {
-        this.userService.addFriend(req.user.name, req.body.username);
-        return req.username + ' is now your friend';
-    }
+  @UseGuards(JwtAuthGuard)
+  @Post('friend')
+  async addFriend(@Request() req: any) {
+    this.userService.addFriend(req.user.name, req.body.username);
+    return req.username + ' is now your friend';
+  }
 
-    @UseGuards(JwtAuthGuard)
-    @Get('friend')
-    async getFriends(@Request() req: any) {
-        return {friends: JSON.stringify(this.userService.getFriends(req.user.name))};
-    }
+  @UseGuards(JwtAuthGuard)
+  @Get('friend')
+  async getFriends(@Request() req: any) {
+    return { friends: JSON.stringify(this.userService.getFriends(req.user.name)) };
+  }
 
-    @UseGuards(JwtAuthGuard)
-    @Delete('friend')
-    async removeFriend(@Request() req: any) {
-        this.userService.removeFriend(req.user.name, req.body.username)
-        return req.username + ' is no longer your friend';
-    }
+  @UseGuards(JwtAuthGuard)
+  @Delete('friend')
+  async removeFriend(@Request() req: any) {
+    this.userService.removeFriend(req.user.name, req.body.username);
+    return req.username + ' is no longer your friend';
+  }
 
-    @UseGuards(JwtAuthGuard)
-    @Get('info/:username')
-    async getInfo(@Param('username') username: string) {
-        return {userInfo: this.userService.getInfo(username)};
-    }
+  @UseGuards(JwtAuthGuard)
+  @Get('info/:username')
+  async getInfo(@Param('username') username: string) {
+    return { userInfo: this.userService.getInfo(username) };
+  }
 
-    @UseGuards(JwtAuthGuard)
-    @Get('info')
-    async getInfoAboutSelf(@Request() req: any) {
-        return {userInfo: this.userService.getInfo(req.user.name)};
-    }
+  @UseGuards(JwtAuthGuard)
+  @Get('info')
+  async getInfoAboutSelf(@Request() req: any) {
+    return { userInfo: this.userService.getInfo(req.user.name) };
+  }
 
-    @UseGuards(JwtAuthGuard)
-    @Get('')
-    async getAllUserNames(@Request() req: any) {
-        return {usernames: this.userService.getAllUsernames()};
-    }
+  @UseGuards(JwtAuthGuard)
+  @Get('channels')
+  async getChannels(@Request() req: any) {
+    return { channels: await this.userService.getChannels(req.user.name) };
+  }
 
-    @UseGuards(JwtAuthGuard)
-    @Get('channels')
-    async getChannels(@Request() req: any) {
-        return {channels: await this.userService.getChannels(req.user.name)};
-    }
+  @UseGuards(JwtAuthGuard)
+  @Get('getMatchingNames/:regexString')
+  async findMatching(@Param() params: any) {
+    const matchingUsernames = await this.userService.findMatching(
+      params.regexString
+    );
+    return { usernames: matchingUsernames };
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('getMatchingNames')
+  async findAllUserNames(@Param() params: any) {
+    const matchingUsernames = await this.userService.findMatching('');
+    return { usernames: matchingUsernames };
+  }
 }
