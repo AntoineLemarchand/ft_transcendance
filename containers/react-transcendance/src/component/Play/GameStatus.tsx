1b341614e70e4cc2dd5d61d9faadbe6f1685a2c6
ThePush <jeremysemel@gmail.com>
2023-01-05 15:31:57 +0100

    feat(react-transcendance): game and scores working in local

--- containers/react-transcendance/src/component/Play/GameStatus.tsx
+++ containers/react-transcendance/src/component/Play/GameStatus.tsx
@@ -1,43 +1,59 @@
-import 'static/Play/GameStatus.scss'
+import "static/Play/GameStatus.scss";
 
-function GameStatus() {
-	const gameStatus = {
-		players: ["Jaydee", "some random dude"],
-		scores: [10, 2]
-	}
+type GameStatusProps = {
+  score: {
+    player1: number;
+    player2: number;
+  };
+  updateScore: (player: "player1" | "player2", value: number) => void;
+};
 
-	const PlaceholderPic1 = "https://cdn.intra.42.fr/users/e67b5f138e3ee505e5180dba55a55ac5/dpaccagn.jpg"
-	const PlaceholderPic2 = "https://cdn.intra.42.fr/users/26f8442a6176644e8c7fa706d568d790/alemarch.jpg"
+function GameStatus({ score, updateScore }: GameStatusProps) {
+  const gameStatus = {
+    players: ["Jaydee", "some random dude"],
+  };
 
-	const scoreStyle = (score0: number, score1: number) => {
-		if (score0 > score1) {
-			return {background: "#b8bb26"}
-		} else if (score0 !== score1) {
-			return {background: "#cc241d"}
-		} else {
-			return {background: "#d3869b"}
-		}
-	}
+  const PlaceholderPic1 =
+    "https://cdn.intra.42.fr/users/e67b5f138e3ee505e5180dba55a55ac5/dpaccagn.jpg";
+  const PlaceholderPic2 =
+    "https://cdn.intra.42.fr/users/26f8442a6176644e8c7fa706d568d790/alemarch.jpg";
+
+  const scoreStyle = (scoreP1: number, scoreP2: number) => {
+    if (scoreP1 > scoreP2) {
+      return { background: "#b8bb26" };
+    } else if (scoreP1 !== scoreP2) {
+      return { background: "#cc241d" };
+    } else {
+      return { background: "#d3869b" };
+    }
+  };
+
   return (
     <div className="GameStatus">
       <div className="player player1">
-        <img className="picture" src={PlaceholderPic1}
-        alt={gameStatus.player1} />
+        <img
+          className="picture"
+          src={PlaceholderPic1}
+          alt={gameStatus.players[0]}
+        />
         <p className="name">{gameStatus.players[0]}</p>
-        <div className="score"
-          style={scoreStyle(gameStatus.scores[0], gameStatus.scores[1])}
-        >{gameStatus.scores[0]}</div>
+        <div className="score" style={scoreStyle(score.player1, score.player2)}>
+          {score.player1}
+        </div>
       </div>
       <div className="player player2">
-        <img className="picture" src={PlaceholderPic2}
-        alt={gameStatus.player2} />
+        <img
+          className="picture"
+          src={PlaceholderPic2}
+          alt={gameStatus.players[1]}
+        />
         <p className="name">{gameStatus.players[1]}</p>
-        <div className="score"
-          style={scoreStyle(gameStatus.scores[1], gameStatus.scores[0])}
-        >{gameStatus.scores[1]}</div>
+        <div className="score" style={scoreStyle(score.player2, score.player1)}>
+          {score.player2}
+        </div>
       </div>
     </div>
-  )
+  );
 }
 
 export default GameStatus;
