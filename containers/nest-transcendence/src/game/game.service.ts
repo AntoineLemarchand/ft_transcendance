39ad0f2b99bb8ace4b5c650b0267075b0c36464e
Frederik Schlüter <frederik.schlueter@posteo.de>
2023-01-14 19:35:55 +0100

    refactor(game.module.ts): clean up service even more

--- containers/nest-transcendence/src/game/game.service.ts
+++ containers/nest-transcendence/src/game/game.service.ts
@@ -2,6 +2,7 @@ import { forwardRef, Inject, Injectable } from '@nestjs/common';
 import { UserService } from '../user/user.service';
 import { GameObjectRepository } from './game.currentGames.repository';
 import { BroadcastingGateway } from '../broadcasting/broadcasting.gateway';
+import { GameObject } from './game.entities';
 @Injectable()
 export class GameService {
   constructor(
@@ -13,7 +14,7 @@ export class GameService {
 
   //todo: add exception filter -> transform Error to HTTPException
   async initGame(player1name: string, player2name: string) {
-    await this.prohibitNonActivePlayerActions(player1name, player2name);
+    await this.areValidPlayers(player1name, player2name);
     const result = this.currentGames.create(player1name, player2name);
     await this.createRoom(player1name, result.getId(), player2name);
     return result;
@@ -25,17 +26,21 @@ export class GameService {
 
   async setReady(executorName: string, gameId: number) {
     const game = await this.currentGames.findOne(gameId);
+    await this.prohibitNonPlayerActions(executorName, game);
+    game.setReady(executorName);
+  }
+
+  private async prohibitNonPlayerActions(
+    executorName: string,
+    game: GameObject,
+  ) {
     if (!game.getPlayerNames().find((name) => name === executorName))
       return Promise.reject(
         new Error('only active players can set themselves as ready'),
       );
-    game.setReady(executorName);
   }
 
-  private async prohibitNonActivePlayerActions(
-    player1name: string,
-    player2name: string,
-  ) {
+  private async areValidPlayers(player1name: string, player2name: string) {
     if (player1name === player2name)
       return Promise.reject(new Error('a player cannot play against himself'));
     if (
