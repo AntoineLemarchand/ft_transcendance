d7ae838971e31c96789caff1a093873544ccd55e
ThePush <jeremysemel@gmail.com>
2023-01-20 15:38:17 +0100

    feat(Websocket.tsx, Login.tsx): UserInfo in context

--- containers/react-transcendence/src/component/WebSocket.tsx
+++ containers/react-transcendence/src/component/WebSocket.tsx
@@ -1,41 +1,54 @@
 import { createContext, useState } from "react";
-import { io, Socket } from 'socket.io-client';
+import { io, Socket } from "socket.io-client";
 
-const initialAuth: string | undefined = undefined
-const initialSocket: Socket | undefined = undefined
+const initialAuth: string | undefined = undefined;
+const initialSocket: Socket | undefined = undefined;
+const initialUserInfo: {} | undefined = undefined;
 
 interface ContextProps {
   auth: string | undefined;
   setAuth: React.Dispatch<React.SetStateAction<string | undefined>>;
   socket: Socket | undefined;
   initSocket: Function;
+  userInfo: {} | undefined;
+  setUserInfo: React.Dispatch<React.SetStateAction<{} | undefined>>;
 }
 
-export const SocketContext = createContext<ContextProps>(
-{
-  auth: '',
+export const SocketContext = createContext<ContextProps>({
+  auth: "",
   setAuth: () => {},
   socket: undefined,
   initSocket: () => {},
+  userInfo: undefined,
+  setUserInfo: () => {},
 });
 
-export const SocketProvider = (props: {children: React.ReactNode}) => {
+export const SocketProvider = (props: { children: React.ReactNode }) => {
   const [auth, setAuth] = useState(initialAuth);
   const [socket, setSocket] = useState(initialSocket);
+  const [userInfo, setUserInfo] = useState(initialUserInfo);
 
   const initSocket = () => {
     if (auth) {
-      const newSocket = io('http://' + process.env.REACT_APP_SERVER_IP, {
-          withCredentials: true,
-          query: {auth: auth},
-        })
+      const newSocket = io("http://" + process.env.REACT_APP_SERVER_IP, {
+        withCredentials: true,
+        query: { auth: auth },
+      });
       setSocket(newSocket);
     }
-  }
+  };
 
   return (
     <SocketContext.Provider
-      value={{auth, setAuth, socket, initSocket}}>
+      value={{
+        auth,
+        setAuth,
+        socket,
+        initSocket,
+        userInfo,
+        setUserInfo,
+      }}
+    >
       {props.children}
     </SocketContext.Provider>
   );
