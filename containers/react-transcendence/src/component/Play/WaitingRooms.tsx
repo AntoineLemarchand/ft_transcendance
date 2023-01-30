b6072b841c126e8065df684e5d48fee908a92896
ThePush <jeremysemel@gmail..com>
2023-01-30 11:19:13 +0100

    feat(Game): begin hardcore mode

--- containers/react-transcendence/src/component/Play/WaitingRooms.tsx
+++ containers/react-transcendence/src/component/Play/WaitingRooms.tsx
@@ -1,54 +1,53 @@
-import { useState, useEffect, useContext } from "react";
+import { useState, useEffect } from "react";
 import { useParams, useNavigate } from "react-router-dom";
-import { useCookies } from 'react-cookie'
-import { Socket, io } from "socket.io-client"
+import { Socket } from "socket.io-client";
 import "static/Play/WaitingRoom.scss";
-import Game from './Game'
+import Game from "./Game";
 
-export function PreMatchRoom(props: {socket: Socket}) {
+export function PreMatchRoom(props: { socket: Socket }) {
   const [userReady, setUserReady] = useState(false);
-  const [gameStart, setGameStart] = useState('');
+  const [gameStart, setGameStart] = useState("");
   const [currentGamemode, setCurrentGamemode] = useState("Normal");
-  const [cookies] = useCookies(['auth']);
-	const params = useParams();
+  const params = useParams();
   const navigate = useNavigate();
 
   const GamemodeButtonStyle = (gamemode: string) => {
-    return gamemode === currentGamemode ?
-      {background: "#83a598", border: "inset",} : {background: "#458588",};
+    return gamemode === currentGamemode
+      ? { background: "#83a598", border: "inset" }
+      : { background: "#458588" };
   };
 
   const PlayerReadyButton = (isReady: boolean) => {
-    return isReady ?
-      {background: "#b8bb26", border: "inset",} : {background: "#cc241d",};
+    return isReady
+      ? { background: "#b8bb26", border: "inset" }
+      : { background: "#cc241d" };
   };
 
   useEffect(() => {
-    fetch('http://' + process.env.REACT_APP_SERVER_IP + '/api/game/setReady', {
-      credentials: 'include',
-      method: userReady ? 'POST' : 'DELETE',
+    fetch("http://" + process.env.REACT_APP_SERVER_IP + "/api/game/setReady", {
+      credentials: "include",
+      method: userReady ? "POST" : "DELETE",
       headers: {
-        'Content-type': 'application/json; charset=UTF-8',
+        "Content-type": "application/json; charset=UTF-8",
       },
       body: JSON.stringify({
-        'gameId': params.gid,
+        gameId: params.gid,
       }),
     }).then((response) => {
-      if (response.status === 404)
-        navigate('/game');
-    })
-  }, [userReady])
+      if (response.status === 404) navigate("/game");
+    });
+  }, [userReady]);
 
   useEffect(() => {
     const messageListener = (payload: string) => {
       setGameStart(payload);
-    }
-    props.socket.on("gameUpdateToClient", messageListener)
+    };
+    props.socket.on("gameUpdateToClient", messageListener);
     //return (() => {props.socket.off("gameUpdateToClient", messageListener)})
-  }, [])
+  }, []);
 
-  if (gameStart != '') {
-    return (<Game firstMove={gameStart} socket={props.socket}/>)
+  if (gameStart != "") {
+    return <Game firstMove={gameStart} socket={props.socket} />;
   }
   return (
     <div className="waitingRoom">
@@ -56,19 +55,22 @@ export function PreMatchRoom(props: {socket: Socket}) {
         <div className="Gamemode">
           <button
             onClick={() => setCurrentGamemode("Normal")}
-            style={GamemodeButtonStyle("Normal")}>
+            style={GamemodeButtonStyle("Normal")}
+          >
             Normal
           </button>
           <button
             onClick={() => setCurrentGamemode("Hardcore")}
-            style={GamemodeButtonStyle("Hardcore")}>
+            style={GamemodeButtonStyle("Hardcore")}
+          >
             Hardcore
           </button>
         </div>
         <div className="PlayerStatus">
           <button
             onClick={() => setUserReady(!userReady)}
-            style={PlayerReadyButton(userReady)}>
+            style={PlayerReadyButton(userReady)}
+          >
             Ready
           </button>
         </div>
@@ -90,24 +92,27 @@ export function MatchMakingRoom(props: { socket: Socket }) {
 
   useEffect(() => {
     const messageListener = (payload: string) => {
-      console.log(payload)
-      navigate('/game/' + payload);
-    }
-    props.socket.on("emitMatchMadeToClient", messageListener)
-    return ( () => {
-      props.socket.off("emitMatchMadeToClient", messageListener)
-    })
-  }, [])
+      console.log(payload);
+      navigate("/game/" + payload);
+    };
+    props.socket.on("emitMatchMadeToClient", messageListener);
+    return () => {
+      props.socket.off("emitMatchMadeToClient", messageListener);
+    };
+  }, []);
 
   useEffect(() => {
-    fetch('http://' + process.env.REACT_APP_SERVER_IP + '/api/game/matchMaking', {
-      credentials: 'include',
-      method: 'POST',
-      headers: {
-        'Content-type': 'application/json; charset=UTF-8',
-      },
-    })
-  }, [])
+    fetch(
+      "http://" + process.env.REACT_APP_SERVER_IP + "/api/game/matchMaking",
+      {
+        credentials: "include",
+        method: "POST",
+        headers: {
+          "Content-type": "application/json; charset=UTF-8",
+        },
+      }
+    );
+  }, []);
 
   return (
     <div className="waitingRoom">
@@ -117,4 +122,3 @@ export function MatchMakingRoom(props: { socket: Socket }) {
     </div>
   );
 }
-
