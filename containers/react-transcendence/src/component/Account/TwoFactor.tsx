98ffa1c33ba13cb3de07bfc8aed178e6c6bec6c9
AntoineLemarchand <antoine.lemarchand05@gmail.com>
2023-02-01 12:10:35 +0100

    feat(TwoFactor.tsx): Plug init

--- containers/react-transcendence/src/component/Account/TwoFactor.tsx
+++ containers/react-transcendence/src/component/Account/TwoFactor.tsx
@@ -1,8 +1,42 @@
 import 'static/Account/Prompt.scss';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 function TwoFactor() {
   const [twoFaStatus, setTwoFaStatus] = useState(false);
+  const [qrCode, setQrCode] = useState('');
+
+  const ToggleTwoFa = (event) => {
+    setTwoFaStatus(event.target.checked);
+    if (event.target.checked) {
+      fetch("http://" + process.env.REACT_APP_SERVER_IP + '/api/2fa/activate', {
+        credentials: "include",
+        method: "GET",
+      }).then(response => {
+        response.blob().then( (blob: Blob) => {
+          const fileReader = new FileReader();
+          fileReader.onload = (event) => {
+              setQrCode(event.target!.result as string);
+          }
+          fileReader.readAsDataURL(blob);
+        })
+      })
+    } else {
+      fetch("http://" + process.env.REACT_APP_SERVER_IP + '/api/2fa/deactivate', {
+        credentials: "include",
+        method: "GET",
+      })
+      setQrCode('');
+    }
+  }
+
+  useEffect(() => {
+      fetch("http://" + process.env.REACT_APP_SERVER_IP + '/api/2fa/status', {
+        credentials: "include",
+        method: "GET",
+      }).then(response => {
+        response.text().then(text => setTwoFaStatus(JSON.parse(text).status))
+      })
+  })
 
   return (
     <div className="Prompt">
@@ -11,12 +45,13 @@ function TwoFactor() {
         <label className="switch">
           <input
             type="checkbox"
-            onChange={(event)=>setTwoFaStatus(event.target.checked)}
+            onChange={ToggleTwoFa}
+            checked={twoFaStatus}
           />
           <span className="slider"></span>
         </label>
       </header>
-      <img src="" alt={twoFaStatus ? 'activated' : 'deactivated'} />
+      <img src={qrCode} alt={twoFaStatus ? 'activated' : 'deactivated'} />
     </div>
   )
 }
